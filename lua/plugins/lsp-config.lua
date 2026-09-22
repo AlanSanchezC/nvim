8fc7794fa016d88fed38ab6a89862b7ba4603b34
AlanSanchezC <acazarez58@gmail.com>
2026-09-22 16:57:32 -0600

    config updated for version 0.12.5

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,24 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities }) -- Configurar LSP para Lua
-			lspconfig.ts_ls.setup({ capabilities = capabilities }) -- Configurar LSP para javascript
-			lspconfig.pyright.setup({ capabilities = capabilities })
-			lspconfig.htmx.setup({ capabilities = capabilities })
-			lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+      -- Configure LSP for: Lua, JS, TS, Python, HTMX and Rust
+      vim.lsp.config("*", { capabilities = capabilities })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      local langs = { "lua_ls", "ts_ls", "pyright", "htmx", "rust_analyzer" }
+      vim.lsp.enable(langs)
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,17 +2,20 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
-    --"nvim-lua/plenary.nvim",
+    "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  filesystem = {
-    use_libuv_file_watcher = false, -- Desactiva el monitoreo de archivos con libuv
-    filtered_items = {
-      visible = true,             -- Muestra archivos ocultos globalmente
+  opts = {
+    filesystem = {
+      use_libuv_file_watcher = false, -- Desactiva el monitoreo de archivos con libuv
+      filtered_items = {
+        visible = true, -- Muestra archivos ocultos globalmente
+      },
     },
   },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+    vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Alternar Neo-tree" })
   end,
 }
